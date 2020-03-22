57ecc45e8fa939b22196ba938f7b20b6856e0a55
vipul goyal <vipulgoyal794@gmail.com>
2020-03-22 20:14:53 +0530

    Added backend layer for tds return

--- src/app/return-credentials/tds-return/tds-return.component.ts
+++ src/app/return-credentials/tds-return/tds-return.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {SyncupApiService} from 'src/app/shared/api/syncup-api.service';
-import {FormGroup, FormBuilder} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {ReturnCredentials} from '../../model/ReturnCredentials';
+import {DataTransferService} from '../../shared/data/data-transfer.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-tds-return',
@@ -9,23 +12,41 @@ import {FormGroup, FormBuilder} from '@angular/forms';
 })
 export class TdsReturnComponent implements OnInit {
 
+  private clientId: string;
   private tdsReturnForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private apiService: SyncupApiService) {
-    this.tdsReturnForm = formBuilder.group({
-                    tdsTanNo: [''],
-                    tdsUserName: [''],
-                    rocPassword: [''],
-                    tracesTdsUserName: [''],
-                    tracesTdsPassword: ['']
+  constructor(private formBuilder: FormBuilder, private apiService: SyncupApiService, private dataTransferService: DataTransferService, private router: Router) {
+    this.tdsReturnForm = this.formBuilder.group({
+                    tdsTanNo: [null, [Validators.required]],
+                    tdsUserName: [null, [Validators.required]],
+                    tdsPassword: [null, [Validators.required]],
+                    tracesTdsUserName: [null, [Validators.required]],
+                    tracesTdsPassword: [null, [Validators.required]]
                   });
    }
 
   ngOnInit() {
+    this.dataTransferService.currentMessage.subscribe(message => this.clientId = message);
   }
 
   private addTdsReturnInfo() {
     console.log(this.tdsReturnForm.value);
+    const tdsReturnCredentials: ReturnCredentials = new ReturnCredentials();
+    tdsReturnCredentials.setUserId = this.tdsReturnForm.controls.tdsUserName.value;
+    tdsReturnCredentials.setPassword = this.tdsReturnForm.controls.tdsPassword.value;
+    tdsReturnCredentials.setTracesUserId = this.tdsReturnForm.controls.tracesTdsUserName.value;
+    tdsReturnCredentials.setTracesPassword = this.tdsReturnForm.controls.tracesTdsPassword.value;
+    tdsReturnCredentials.setTanNo = this.tdsReturnForm.controls.tdsTanNo.value;
+    tdsReturnCredentials.setId = +this.clientId;
+    tdsReturnCredentials.setReturnType = "tds";
+    this.apiService.addReturnCredentials(tdsReturnCredentials).subscribe(
+      res => {
+        console.log(tdsReturnCredentials + " insertion successful")
+      },
+      err => {
+        alert('oops!!! Somthing went wrong');
+      }
+    );
   }
 
 }
